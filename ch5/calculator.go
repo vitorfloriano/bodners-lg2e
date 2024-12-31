259218bd9d39e45093ae7dd5463a6d769feebe00
Vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-12-31 11:15:40 -0300

    Add calculator example from chapter 5
    This commit adds the example that demonstrates how to make a calculator using functions as values in a map. Interesting stuff.

---
+++ ch5/calculator.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func add(i int, j int) int { return i + j }
+func sub(i int, j int) int { return i - j }
+func mul(i int, j int) int { return i * j }
+func div(i int, j int) int { return i / j }
+
+var opMap = map[string]func(int, int) int {
+	"+": add,
+	"-": sub,
+	"*": mul,
+	"/": div,
+}
+
+func main() {
+	expressions := [][]string{
+		{"2", "+","3"},
+		{"2", "-","3"},
+		{"2", "*","3"},
+		{"2", "/","3"},
+		{"2", "%","3"},
+		{"two", "+","three"},
+		{"5"},
+}
+
+	for _, expression := range expressions {
+		if len(expression) != 3 {
+			fmt.Println("invalid expression:", expression)
+			continue
+		}
+		p1, err := strconv.Atoi(expression[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		op := expression[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("unsuported operator:", op)
+			continue
+		}
+		p2, err := strconv.Atoi(expression[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		result := opFunc(p1, p2)
+		fmt.Println(result)
+	}
+}	
