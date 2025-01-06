6ee2a290bbe1d280983705ef2bc6fddf8935ba4a
Vitor <107767584+vitorfloriano@users.noreply.github.com>
2025-01-06 12:01:57 -0300

    Add exercises to chapter 6
    This commit adds the exercises for the chapter 6. It was a tough one. Had to lookup all the solutions. My head aches. Pointers and garbage collection are no kidding.

---
+++ ch6/ex3.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	FirstName string
+	LastName string
+	Age int
+}
+
+func MakePerson(firstName, lastName string, age int) Person {
+	p := Person{firstName, lastName, age}
+	return p
+}
+
+func MakePersonPointer(firstName, lastName string, age int) *Person {
+	p := &Person{firstName, lastName, age}
+	return p
+}
+
+func UpdateSlice(strsl []string, str string) {
+	strsl[len(strsl)-1] = str
+	fmt.Println("in UpdateSlice: ", strsl)
+}	
+
+func GrowSlice(strsl []string, str string) {
+	strsl = append(strsl, str)
+	fmt.Println("in GrowSlice: ", strsl)
+}
+
+func main() {
+	p := MakePerson("vitor", "floriano", 32)
+	fmt.Println(p)
+	p2 := MakePersonPointer("joao", "pedro", 34)	
+	fmt.Println(p2)
+
+	slice := []string{"1","2","3"}
+	fmt.Println("Current slice: ", slice)
+	UpdateSlice(slice, "4")
+	fmt.Println("After UpdateSlice: ", slice)
+	GrowSlice(slice, "4")
+	fmt.Println("After GrowSlice: ", slice)
+
+	var slicePerson []Person
+	for i := 0; i <= 10_000_000; i++ {
+		slicePerson = append(slicePerson, MakePerson("vitor","floriano", 32))			
+	}
+}
