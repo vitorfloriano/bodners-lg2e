ff86b7c3cbe57eb2ce482cc940a3fdba9f425c48
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2025-01-13 22:54:37 -0300

    Update reading progress and Add Exercise
    This commit adds the exercise for chapter seven and
    updates the reading progress as well. It's a WIP.

--- ch7/ex1.go
+++ ch7/ex1.go
@@ -5,18 +5,48 @@ import (
 )
 
 type Team struct {
-	teamName   string
-	playerName string
+	teamName    string
+	playerNames []string
 }
 
 type League struct {
-	Teams Team
+	Teams []Team
 	Wins  map[string]int
 }
 
-func (l League) MatchResult(firstTeam, secondTeam string, ftScore, stScore int) {
-	
+func (l *League) MatchResult(firstTeam, secondTeam string, ftScore, stScore int) {
+	firstTeamExists := false
+	for _, team := range l.Teams {
+		if team.teamName == firstTeam {
+			firstTeamExists = true
+			break
+		}
+	}
+	secondTeamExists := false
+	for _, team := range l.Teams {
+		if team.teamName == secondTeam {
+			secondTeamExists = true
+			break
+		}
+	}
+	if !firstTeamExists || !secondTeamExists {
+		fmt.Println("One or both teams are not in the league.")
+		return
+	}
+	if ftScore > stScore {
+		l.Wins[firstTeam]++
+	}
+	else if stScore > ftScore {
+		l.Wins[secondTeam]++
+	} else {
+		// do nothing
+	}
+	}
 }
+	
+
+}
+
 
 func (l League) Ranking() []Team.teamName {
 	
