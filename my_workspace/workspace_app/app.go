146043b7b2a8072c51a642ac028bd70d522cd2a0
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2025-02-06 23:51:06 -0300

    Add workspace example in chapter 10
    This commmit adds the example in chapter 10 that shows why and how to use workspaces. Still a WIP.

---
+++ my_workspace/workspace_app/app.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vitorfloriano/workspace_lib"
+)
+
+func main() {
+	fmt.Println(workspace_lib.AddNums(2, 3))
+}
---
+++ my_workspace/workspace_lib/go.mod
@@ -0,0 +1,3 @@
+module github.com/vitorfloriano/workspace_lib
+
+go 1.23.2
