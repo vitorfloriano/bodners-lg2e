fe11d0ec553013ebff7bb7c507ca98d3a4b8feab
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2025-02-18 22:30:36 -0300

    Add two files and embed the content in variables
    This commit adds two files containing the UDHR in
    English and Portuguese, then embeds their contents
    in a package level variable.

--- ch11/udhr/main.go
+++ ch11/udhr/main.go
@@ -3,6 +3,13 @@ package main
 import (
 	"os"
 	"fmt"
+	"io/ioutil"
 )
+
+//go:embed english_rights.txt 
+var englishRights string
+
+//go:embed portuguese_rights.txt
+var portugueseRights string
 
 func main
