de5bd75522cdfa715f2457a3195944b6e45c4489
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2025-02-19 22:55:01 -0300

    Finish exercise 1 in chapter 11
    This finishes the program for the exercise 1
    in chapter 11.
    
    Once again I struggle with simple concepts. Maybe,
    just maybe, I should study harder.

--- ch11/udhr/main.go
+++ ch11/udhr/main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"os"
 	"fmt"
-	"io/ioutil"
+	"embed"
+	"strings"
 )
 
-//go:embed english_rights.txt 
-var englishRights string
+//go:embed english_rights.txt portuguese_rights.txt 
+var files embed.FS
 
-//go:embed portuguese_rights.txt
-var portugueseRights string
+var languages = map[string]string{
+	"english": "english_rights.txt",
+	"portuguese": "portuguese_rights.txt",
+}
 
-func main
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <language>")
+		return
+	}
+
+	language := strings.ToLower(os.Args[1])
+	
+	filename, exists := languages[language]
+	if !exists {
+		fmt.Println("Language not supported")
+		return
+	}
+	
+	content, err := files.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	
+	fmt.Println(string(content))
+}
